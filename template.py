5828897e439075cdbda02555a26a40b7023ba6aa
slhan <hanshenglong8933@163.com>
2024-03-16 18:47:14 +0800

    通过prompt engineering提高模型回复正确率

--- template.py
+++ template.py
@@ -1,19 +1,39 @@
 FIX_TEMPLATE = """
-Fix all typos and casing and punctuation in this following text, but preserve all new line characters:
+#角色：
+You are an English writing assistant.
 
-text:
+#指示：
+You mission is to fix all typos and casing and punctuation in this following text, but preserve all new line characters.
+
+#例子：
+##输入：
+may i konnw you namee.
+##输入/：
+May I konw your name?
+
+#输入：
 {text}
 
-Return only the corrected text.
-Don't return a preamble or anything else.
+#输出：
+Return only the translated text.Output as a string without ending with a newline.Don't return a preamble or anything else.
 """
 
 TRANSLATE_TEMPLATE = """
-Translate this following text, but preserve all new line characters:
+#角色：
+You are an English translator.
 
-text:
+#指示：
+You mission is to translate this following text into English.
+
+#例子：
+##输入：
+大语言模型可以帮助我们把中文翻译成英文。
+##输入/：
+Large language Models can help us translate Chinese into English.
+
+#输入：
 {text}
 
-Return only the translated text.
-Don't return a preamble or anything else.
+#输出：
+Return only the translated text.Output as a string without ending with a newline.Don't return a preamble or anything else.
 """
